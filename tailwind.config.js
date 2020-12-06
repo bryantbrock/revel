0ece2c0a3b38858bd30c8e47d7579ede5516d93a
bryantbrock <bryantleebrock@gmail.com>
2020-12-05 18:21:09 -0800

    default styling config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   purge: ['./src/**/*.js', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      black: '#454545',
+      white: '#ffffff',
+      gray: {
+        100: '#EAEAEA',
+        200: 'EBEBEB',
+      },
+      borderRadius: {
+        'none': '0',
+        'sm': '.25rem',
+        DEFAULT: '.5rem',
+        'lg': '2rem',
+        'full': '9999px',
+      },
+      fontFamily: {
+        sans: [
+          'Open Sans',
+          ...defaultTheme.fontFamily.sans,
+        ]
+      },
+    },
   },
   variants: {
     extend: {},
