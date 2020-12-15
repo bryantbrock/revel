3c05564199bbe3ea423efe479a8906b6ac7ab7dd
bryantbrock <bryantleebrock@gmail.com>
2020-12-15 13:22:43 -0800

    clearing out

--- api/favorites.js
+++
@@ -1,7 +0,0 @@
-module.exports = (req, res) => {
-  res.status(200).json({
-    books: [{rank: 1, name: 'Failure of Nerve', author: 'Edwin Friedman'}],
-    movies: [{rank: 1, name: 'Superman'}],
-    food: [{rank: 1, name: 'Donuts'}],
-  })
-}
